f192a3c117a2d73b998c0256b97402577cc20c5f
intronate67 <huntersharpe@me.com>
2014-07-11 22:43:17 -0500

    Added Stuff... Deal with it Jkcclemens

--- src/main/java/com/tenjava/entries/intronate67/t1/TenJava.java
+++ src/main/java/com/tenjava/entries/intronate67/t1/TenJava.java
@@ -1,6 +1,8 @@
 package com.tenjava.entries.intronate67.t1;
 
 import com.tenjava.entries.intronate67.t1.Economy.EconManager;
+import com.tenjava.entries.intronate67.t1.combat.CombatListener;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,10 +10,17 @@ import java.util.logging.Level;
 
 public class TenJava extends JavaPlugin {
 
+    private static TenJava instance;
+
+    public static TenJava getInstance(){
+        return instance;
+    }
+
     FileConfiguration config = getConfig();
 
     @Override
     public void onEnable(){
+        Bukkit.getPluginManager().registerEvents(new CombatListener(), this);
         if(!getDataFolder().exists()){
             getDataFolder().mkdir();
         }
--- src/main/java/com/tenjava/entries/intronate67/t1/combat/CombatListener.java
+++ src/main/java/com/tenjava/entries/intronate67/t1/combat/CombatListener.java
@@ -1,18 +1,30 @@
 package com.tenjava.entries.intronate67.t1.combat;
 
+import com.tenjava.entries.intronate67.t1.Economy.EconManager;
+import com.tenjava.entries.intronate67.t1.TenJava;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import java.util.Random;
 
 /**
  * Created by Server on 7/11/2014.
  */
 public class CombatListener implements Listener {
 
+    public int n;
     public int expCounter;
 
     @EventHandler
@@ -22,20 +34,46 @@ public class CombatListener implements Listener {
 
     }
     @EventHandler
-    public void onBowUse(EntityShootBowEvent e){
-        if(!(e.getEntity() instanceof Player)){
+    public void onBowUse(ProjectileLaunchEvent e){
+        if(!(e.getEntity().getShooter() instanceof Player)){
             return;
         }
         Player p = ((Player) e.getEntity()).getPlayer();
-
+        if(expCounter <= 0){
+            return;
+        }
+        if(!(e.getEntity() instanceof Arrow)){
+            return;
+        }
+        Arrow arrow = (Arrow) e.getEntity();
+        e.setCancelled(true);
+        p.launchProjectile(Arrow.class).setVelocity(arrow.getVelocity().multiply(expCounter));
+        Location loc = arrow.getLocation();
+        Random rand = new Random(10);
+        int n = rand.nextInt();
+        if(n == 10){
+            Entity tnt = loc.getWorld().spawn(loc, TNTPrimed.class);
+            return;
+        }
+        return;
     }
     @EventHandler
-    public void onPlayerCrouch(PlayerToggleSneakEvent e){
-        Player p = e.getPlayer();
+    public void onPlayerCrouch(final PlayerToggleSneakEvent e){
+        final Player p = e.getPlayer();
         if(!p.getItemInHand().equals(Material.BOW)){
             return;
         }
-
+        n = Bukkit.getScheduler().scheduleSyncRepeatingTask(TenJava.getInstance(), new BukkitRunnable() {
+            @Override
+            public void run() {
+                if(expCounter == 10){
+                    Bukkit.getScheduler().cancelTask(n);
+                    return;
+                }
+                p.setLevel(expCounter);
+                expCounter++;
+            }
+        }, 0L, 20L);
     }
 
 }
