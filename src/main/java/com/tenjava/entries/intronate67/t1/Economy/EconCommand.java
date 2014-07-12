1796a53aea84710e84cc13c7490fb354e7638487
intronate67 <huntersharpe@me.com>
2014-07-12 00:08:08 -0500

    Added Stuff

--- src/main/java/com/tenjava/entries/intronate67/t1/Economy/EconCommand.java
+++ src/main/java/com/tenjava/entries/intronate67/t1/Economy/EconCommand.java
@@ -1,7 +1,32 @@
 package com.tenjava.entries.intronate67.t1.Economy;
 
+import com.tenjava.entries.intronate67.t1.SubCommand;
+import org.bukkit.entity.Player;
+
 /**
  * Created by Server on 7/11/2014.
  */
-public class EconCommand {
+public class EconCommand implements SubCommand{
+
+    public boolean onCommand(Player p, String[] args){
+        if(args.length != 1){
+            p.sendMessage("Nope");
+            return true;
+        }
+        if(!EconManager.hasAccount(p.getUniqueId())){
+            p.sendMessage("You do not have an account!");
+            return true;
+        }
+        double balance = EconManager.getBalance(p.getUniqueId());
+        p.sendMessage("Your balance is " + balance);
+        return true;
+    }
+    public String help(Player p){
+        return "";
+    }
+    public String permission(){
+        return "";
+    }
+
+
 }
---
+++ src/main/java/com/tenjava/entries/intronate67/t1/InventoryGui/InventoryListener.java
@@ -0,0 +1,7 @@
+package com.tenjava.entries.intronate67.t1.InventoryGui;
+
+/**
+ * Created by Server on 7/12/2014.
+ */
+public class InventoryListener {
+}
