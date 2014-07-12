28d1ed39ac5d16d7d78f1016db3c7b31f829596c
intronate67 <huntersharpe@me.com>
2014-07-11 19:43:11 -0500

    Fixed interface methods in Main Command

--- src/main/java/com/tenjava/entries/intronate67/t1/MainCommand.java
+++ src/main/java/com/tenjava/entries/intronate67/t1/MainCommand.java
@@ -1,10 +1,20 @@
 package com.tenjava.entries.intronate67.t1;
 
+import org.bukkit.entity.Player;
+
 /**
  * Created by Server on 7/11/2014.
  */
 public class MainCommand implements SubCommand{
 
-
+    public boolean onCommand(Player p, String[] args){
+        return true;
+    }
+    public String help(Player p){
+        return "";
+    }
+    public String permission(){
+        return "";
+    }
 
 }
