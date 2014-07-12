0c438e7ad85ad0932db2aadaf5f5fda6d2e2f252
intronate67 <huntersharpe@me.com>
2014-07-12 00:20:03 -0500

    Added stuff for inventory gui

--- src/main/java/com/tenjava/entries/intronate67/t1/InventoryGui/InventoryListener.java
+++ src/main/java/com/tenjava/entries/intronate67/t1/InventoryGui/InventoryListener.java
@@ -1,7 +1,30 @@
 package com.tenjava.entries.intronate67.t1.InventoryGui;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
 /**
  * Created by Server on 7/12/2014.
  */
-public class InventoryListener {
+public class InventoryListener implements Listener{
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e){
+
+        if(!e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
+
+        if(e.getPlayer().getItemInHand().equals(Material.COMPASS)) return;
+
+        Player p = e.getPlayer();
+
+        if(!p.hasPermission("abilities.gui")) return;
+
+    }
+
+
+
 }
--- src/main/java/com/tenjava/entries/intronate67/t1/InventoryGui/inventory.java
+++ src/main/java/com/tenjava/entries/intronate67/t1/InventoryGui/inventory.java
@@ -1,7 +1,40 @@
 package com.tenjava.entries.intronate67.t1.InventoryGui;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 /**
  * Created by Server on 7/12/2014.
  */
 public class inventory {
+
+    private boolean fireball;
+    private boolean op;
+    private boolean water;
+    private boolean push;
+    private boolean drop;
+
+    private Inventory gui;
+
+    public void openInventoryGui(Player p, int rows, ItemStack item){
+
+        ItemStack i = item;
+        ItemMeta m = i.getItemMeta();
+        gui = Bukkit.createInventory(p, rows * 9, "Abilities");
+        if(p.hasPermission("abilities.fireball")) fireball = true;
+
+        if(p.hasPermission("abilities.op")) op = true;
+
+        if(p.hasPermission("abilities.water")) water = true;
+
+        if(p.hasPermission("abilities.push")) push = true;
+
+        if(p.hasPermission("abilities.drop")) drop = true;
+
+
+    }
+
 }
