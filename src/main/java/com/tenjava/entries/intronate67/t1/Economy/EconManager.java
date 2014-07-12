603ab75fd49e300d8d445b3176d406f7f240084b
intronate67 <huntersharpe@me.com>
2014-07-11 21:52:00 -0500

    DID A METRIC S**T TON!

--- src/main/java/com/tenjava/entries/intronate67/t1/Economy/EconManager.java
+++ src/main/java/com/tenjava/entries/intronate67/t1/Economy/EconManager.java
@@ -1,7 +1,54 @@
 package com.tenjava.entries.intronate67.t1.Economy;
 
+import com.tenjava.entries.intronate67.t1.TenJava;
+import org.bukkit.Bukkit;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 /**
  * Created by Server on 7/11/2014.
  */
 public class EconManager {
+
+    private static TenJava plugin;
+
+    public EconManager(TenJava plugin){
+        this.plugin = plugin;
+    }
+
+    public static HashMap<UUID, Double> bal = new HashMap<UUID, Double>();
+
+    public static void setBalance(UUID playerId, double amount){
+        bal.put(playerId, amount);
+    }
+    public static double getBalance(UUID playerId){
+        return bal.get(playerId);
+    }
+    public static boolean hasAccount(UUID playerId){
+        return bal.containsKey(playerId);
+    }
+    public static HashMap<UUID, Double> getBalanceMap(){
+        return bal;
+    }
+
+    public static TenJava getPlugin(){
+        return plugin;
+    }
+    public static void getTopBalance(UUID playerId){
+        Map.Entry<UUID, Double> maxEntry = null;
+        for(Map.Entry<UUID, Double> entry : bal.entrySet()){
+            if(maxEntry == null || entry.getValue() > maxEntry.getValue()){
+                maxEntry = entry;
+            }
+        }
+        UUID maxPlayerId = maxEntry.getKey();
+        String playerName = Bukkit.getServer().getPlayer(maxPlayerId).getName();
+        double maxValue = maxEntry.getValue().doubleValue();
+        //TODO: send message.
+        return;
+
+    }
+
 }
--- src/main/java/com/tenjava/entries/intronate67/t1/TenJava.java
+++ src/main/java/com/tenjava/entries/intronate67/t1/TenJava.java
@@ -3,6 +3,8 @@ package com.tenjava.entries.intronate67.t1;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public class TenJava extends JavaPlugin {
 
     FileConfiguration config = getConfig();
@@ -12,6 +14,24 @@ public class TenJava extends JavaPlugin {
         if(!getDataFolder().exists()){
             getDataFolder().mkdir();
         }
+        if(config.getString("mysql.hostname").equals(null)){
+            getLogger().log(Level.WARNING, "Host name not set in config file... disabling plugin.");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+        if(config.getString("mysql.port").equals(null)){
+            getLogger().log(Level.WARNING, "Host name not set in config file... disabling plugin.");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+        if(config.getString("mysql.database").equals(null)){
+            getLogger().log(Level.WARNING, "Host name not set in config file... disabling plugin.");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+        String hostName = config.getString("mysql.hostname");
+        String port = config.getString("mysql.port");
+        String dataBase = config.getString("mysql.database");
+        String user = config.getString("mysql.username");
+        String password = config.getString("mysql.pass");
+        //TODO: Put MYSQL CONSTRUCTOR/STUFF
         saveConfig();
     }
     @Override
