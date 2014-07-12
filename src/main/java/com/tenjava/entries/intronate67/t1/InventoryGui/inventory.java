8ffe96add65062a15adcc4ecd37c26b14fab97f0
intronate67 <huntersharpe@me.com>
2014-07-12 00:30:55 -0500

    Added actual inventory

--- src/main/java/com/tenjava/entries/intronate67/t1/InventoryGui/inventory.java
+++ src/main/java/com/tenjava/entries/intronate67/t1/InventoryGui/inventory.java
@@ -2,39 +2,60 @@ package com.tenjava.entries.intronate67.t1.InventoryGui;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Arrays;
 
 /**
  * Created by Server on 7/12/2014.
  */
 public class inventory {
 
-    private boolean fireball;
-    private boolean op;
-    private boolean water;
-    private boolean push;
-    private boolean drop;
+    private String name;
+    private int size;
+    private Plugin plugin;
+    private String[] optionName;
+    private ItemStack[] optionIcons;
 
-    private Inventory gui;
+    public inventory(String name, int size, Plugin plugin, String[] optionName, ItemStack[] optionIcons){
+        this.name = name;
+        this.size = size;
+        this.plugin = plugin;
+        this.optionName = optionName;
+        this.optionIcons = optionIcons;
+    }
 
-    public void openInventoryGui(Player p, int rows, ItemStack item){
+    public inventory setOption(int position, ItemStack icon, String name, String... info){
+        optionName[position] = name;
+        optionIcons[position] = setItemNameAndLore(icon, name, info);
+        return this;
+    }
+    public void open(Player p){
+        Inventory inv = Bukkit.createInventory(p, size, name);
+        for(int i = 0; i < optionIcons.length; i++){
+            if(optionIcons[i] != null){
+                inv.setItem(i, optionIcons[i]);
+            }
 
-        ItemStack i = item;
-        ItemMeta m = i.getItemMeta();
-        gui = Bukkit.createInventory(p, rows * 9, "Abilities");
-        if(p.hasPermission("abilities.fireball")) fireball = true;
-
-        if(p.hasPermission("abilities.op")) op = true;
-
-        if(p.hasPermission("abilities.water")) water = true;
-
-        if(p.hasPermission("abilities.push")) push = true;
-
-        if(p.hasPermission("abilities.drop")) drop = true;
-
-
+        }
+        p.openInventory(inv);
+    }
+    public void destroy(){
+        HandlerList.unregisterAll();
+        plugin = null;
+        optionName = null;
+        optionIcons = null;
+    }
+    private ItemStack setItemNameAndLore(ItemStack item, String name, String[] lore){
+        ItemMeta im = item.getItemMeta();
+            im.setDisplayName(name);
+            im.setLore(Arrays.asList(lore));
+        item.setItemMeta(im);
+        return item;
     }
 
 }
